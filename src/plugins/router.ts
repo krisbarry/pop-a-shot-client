efc6cb31e8b775ad0e9fdc2411713ec2b60dd92a
Kristopher Barry <kristopherbarry@Chucks-Macbook-Pro.local>
2019-02-27 18:05:23 -0600

    Updates to the scoreboard.

---
+++ src/plugins/router.ts
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/home',
+      // path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/',
+      // path: '/scoreboard',
+      name: 'scoreboard',
+      component: () => import(/* webpackChunkName: "scoreboard" */ '@/views/Scoreboard.vue'),
+    },
+    {
+      path: '/players',
+      name: 'players',
+      component: () => import(/* webpackChunkName: "players" */ '@/views/Players.vue'),
+    },
+    {
+      path: '/statistics',
+      name: 'statistics',
+      component: () => import(/* webpackChunkName: "statistics" */ '@/views/Statistics.vue'),
+    }
+  ],
+})
---
+++ src/views/Statistics.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="statistics view">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  components: {}
+})
+export default class Statistics extends Vue {}
+</script>
